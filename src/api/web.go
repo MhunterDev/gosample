7b0130d6247d4e298d66a96d86c16e2f8fd1900e
matthew hunter <matthew.hunter@plixer.com>
2023-12-07 19:37:39 -0500

    .

--- src/api/web.go
+++ src/api/web.go
@@ -4,9 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Profile struct {
+	Listen-port `json:"listen-port" form:"listen-port" xml:"listen-port" binding:"required`
+	Destination-ip `json:"destination-ip" form:"destination-ip" xml:"destination-ip" binding:"required"`
+	Destination-port `json:"destination-port" form:"destination-port" xml:"destination-port" binding:"required"`
+}
+
+func addProfile(c *gin.Context){
+	
+}
+
 func Router() {
 
 	router := gin.Default()
+
+	router.POST("/", addProfile)
 
 	router.Run(":8080")
 }
